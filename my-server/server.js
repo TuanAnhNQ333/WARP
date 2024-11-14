bfe1006cd2f365e79614c9248a68b58a066c40c5
Tuan Anh <tuananh030305@gmail.com>
2024-11-14 14:05:50 +0700

    main

--- my-server/server.js
+++ my-server/server.js
@@ -1,32 +1,22 @@
-// Import Express
 const express = require('express');
-
-// Khởi tạo ứng dụng Express
+const bodyParser = require('body-parser');
 const app = express();
 
-// Thiết lập cổng
+// Cấu hình body-parser để xử lý dữ liệu từ form
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Thiết lập EJS làm view engine
+app.set('view engine', 'ejs');
+
+// Cấu hình cổng
 const PORT = process.env.PORT || 3000;
 
-// Định nghĩa một route cơ bản
+// Route trang chính
 app.get('/', (req, res) => {
-  res.send('Hello, WARP!');
+  res.send('Welcome to the home page');
 });
 
-// Lắng nghe kết nối trên cổng đã thiết lập
+// Khởi động server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-app.use(express.json());
-app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-  });
-app.get('/about', (req, res) => {
-    res.send('About Page');
-  });
-  
-app.post('/data', (req, res) => {
-    res.json({ message: 'Data received', data: req.body });
-  });
-  
