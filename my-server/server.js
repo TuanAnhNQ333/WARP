b30235f916c4b3227ff0ad46adab51ac01c60025
Tuan Anh <tuananh030305@gmail.com>
2024-11-14 13:44:33 +0700

    mian

--- my-server/server.js
+++ my-server/server.js
@@ -9,10 +9,24 @@ const PORT = process.env.PORT || 3000;
 
 // Định nghĩa một route cơ bản
 app.get('/', (req, res) => {
-  res.send('Hello, warp!');
+  res.send('Hello, WARP!');
 });
 
 // Lắng nghe kết nối trên cổng đã thiết lập
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+  });
+app.get('/about', (req, res) => {
+    res.send('About Page');
+  });
+  
+app.post('/data', (req, res) => {
+    res.json({ message: 'Data received', data: req.body });
+  });
+  
