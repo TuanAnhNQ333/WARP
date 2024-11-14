2692377b48a7ddc6723d3111f69c1821972de1bd
Tuan Anh <tuananh030305@gmail.com>
2024-11-15 05:40:20 +0700

    main

--- L2.js
+++ L2.js
@@ -7,6 +7,35 @@ var lastname = ' anh';
 console.log(firstname + lastname);
 
 
+// boolean
+
+var a = 1;
+var b = 2;
 
 
+var isSuccess = a > b;
 
+console.log(isSuccess);
+
+// if else
+if (a > b)
+{
+    console.log(true);
+}
+else
+{
+    console.log(false);
+}
+var c = 3;
+if(a || 0 && b || 0 && c || 0) 
+{
+    console.log('true');
+}
+else if(a != 0)
+{
+    console.log('true');
+}
+/*
+kieu du lieu : number , string , boolean , underfined , null , symbol , 
+phuc tap : function , object 
+ */
