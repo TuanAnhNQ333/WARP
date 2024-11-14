9090a64af5ecff78e3b6912de06b058754154f49
Tuan Anh <tuananh030305@gmail.com>
2024-11-14 14:16:03 +0700

    server

--- my-server/server.js
+++ my-server/server.js
@@ -13,10 +13,31 @@ const PORT = process.env.PORT || 3000;
 
 // Route trang chính
 app.get('/', (req, res) => {
-  res.send('Welcome to the home page');
+  res.send('Welcome to the home page')``;
 });
 
 // Khởi động server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
+// Route trang đăng nhập
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  // Thực hiện xác thực người dùng
+  res.send(`Logged in as ${username}`);
+});
+
+// Route trang đăng ký
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+app.post('/signup', (req, res) => {
+  const { username, password } = req.body;
+  // Lưu thông tin người dùng mới vào cơ sở dữ liệu (giả sử)
+  res.send(`User ${username} signed up successfully!`);
 });
