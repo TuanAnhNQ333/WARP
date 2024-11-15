f0cc5e572502c8565a9dd4973441ee37368f201d
Tuan Anh <tuananh030305@gmail.com>
2024-11-15 18:48:09 +0700

    serfer

--- object/object.js
+++ object/object.js
@@ -21,3 +21,39 @@ typeof strObject;
 strObject instanceof String;
 Object.prototype.toString.call( strObject); // object string
 
+
+let myObj = {
+    int_prop: 5,
+    str_prop: "hihihi",
+    obj_prop: new Date(),
+    inner_obj: {
+        int_prop: 6
+    },
+    func_prop: function() {
+        console.log("hiihi");
+    }
+};
+console.log(myObj.int_prop);
+console.log(myObj.str_prop);
+console.log(myObj.obj_prop.toLocaleDateString());
+console.log(myObj.inner_obj.int_prop);
+myObj.func_prop();
+
+// define function exprilitly
+function toGreet() {
+    console.log("hihihi");
+}
+let myObject = {
+    greet: toGreet,
+    bywhom: function() {
+        console.log(" - hihihi.org");
+    }
+}
+myObject.greet();
+myObject.bywhom();
+
+
+
+
+
+
